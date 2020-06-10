bce6757e5feed2057b43392e6f44acba7942659c
unknown <jiangfeifan8@163.com>
2020-06-10 21:58:52 +0800

    tip

--- temp/declarations/cc.d.ts
+++ temp/declarations/cc.d.ts
@@ -1,1 +1,1 @@
-/// <reference path="D:\project\CocosDashboard\resources\.editors\Creator3D\1.1.0\resources\resources\3d\engine\bin\.declarations\cc.d.ts"/>
+/// <reference path="D:\client\CocosDashboard\resources\.editors\Creator3D\1.1.0\resources\resources\3d\engine\bin\.declarations\cc.d.ts"/>
